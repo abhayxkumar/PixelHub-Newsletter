9a3244134907718c79128cbd34c93139b63e9dfb
Abhay Kumar <singh.abhay1144@gmail.com>
2022-03-25 20:22:24 +0530

    signup page complete

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+});
+
