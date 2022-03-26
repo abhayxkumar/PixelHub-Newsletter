d09cba7c0aeca9e83fa31214ecb5405f8ee8ed14
Abhay Kumar <singh.abhay1144@gmail.com>
2022-03-26 14:29:46 +0530

    Procfile added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000");
 });
 
